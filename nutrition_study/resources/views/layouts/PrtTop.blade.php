517946f6867845c2f51b72218a35ba8130802fa7
haruya46 <pazu70@gmail.com>
2024-04-06 04:23:14 +0900

    no message

--- nutrition_study/resources/views/layouts/PrtTop.blade.php
+++ nutrition_study/resources/views/layouts/PrtTop.blade.php
@@ -11,8 +11,9 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     {{-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
-    <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet">
-    <link href="{{asset("https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap")}}" rel="stylesheet">
+    {{-- <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset("https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap")}}" rel="stylesheet"> --}}
+    <link href="{{secure_asset('/assets/css/reset.css')}}" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
