2ca17685766bc79c67ad1b1d1c99cadf80e7a79e
haruya46 <pazu70@gmail.com>
2024-04-06 05:58:59 +0900

    no message

--- nutrition_study/resources/views/layouts/PrtTop.blade.php
+++ nutrition_study/resources/views/layouts/PrtTop.blade.php
@@ -11,7 +11,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/css/style.css') }}">
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <meta name="description" content="動画編集を依頼するならLCE 個人事業主の特権でどこよりもローコストで出来ます！まずはお問い合わせ！">
